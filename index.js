581b185fe7710d4cbe52f4878300c8468b3a0257
Titus Wormer <tituswormer@gmail.com>
2021-08-12 14:35:12 +0200

    Use ESM

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-'use strict'
+import retextSentenceSpacing from 'retext-sentence-spacing'
+import retextQuotes from 'retext-quotes'
+import retextDiacritics from 'retext-diacritics'
+import retextRedundantAcronyms from 'retext-redundant-acronyms'
+import retextRepeatedWords from 'retext-repeated-words'
+import retextIndefiniteArticle from 'retext-indefinite-article'
+import retextContractions from 'retext-contractions'
 
 exports.plugins = [
-  [require('retext-sentence-spacing'), {preferred: 2}],
-  [require('retext-quotes'), {preferred: 'smart'}],
-  require('retext-diacritics'),
-  require('retext-redundant-acronyms'),
-  require('retext-repeated-words'),
-  require('retext-indefinite-article'),
-  require('retext-contractions')
+  [retextSentenceSpacing, {preferred: 2}],
+  [retextQuotes, {preferred: 'smart'}],
+  retextDiacritics,
+  retextRedundantAcronyms,
+  retextRepeatedWords,
+  retextIndefiniteArticle,
+  retextContractions
 ]
